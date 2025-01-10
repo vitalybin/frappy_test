d53b400299528af4209a1f3e55f7b7bf0180650c
vitalybin <104634000+vitalybin@users.noreply.github.com>
2025-01-10 03:39:44 +0100

    MOD: try to fix

--- cfg/simple_node_cfg.py
+++ cfg/simple_node_cfg.py
@@ -1,9 +1,10 @@
-Node('simple_node',  # a globally unique identification
-     'Simplenode for test purpose\n\n',  # describes the node
+Node('simple_node',
+     'Simplenode for test purpose\n\n'
+     'Collection of functionalities needed for the analysis of the gas after and before the catalytic process.',
       'tcp://10804',
     
 )  # you might choose any port number > 1024
-Mod('simplenode',  # the name of the module
+Mod('simplen',  # the name of the module
     'frappy_Rachit.simple_node.SimpleNode',  # the class used for communication
     'SimpleNode',  # a description
     value= 1  # the serial connection
