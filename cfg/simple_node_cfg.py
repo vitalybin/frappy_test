4e46432191e150a5e723b9955c0faceac29fc301
vitalybin <104634000+vitalybin@users.noreply.github.com>
2025-01-10 03:27:23 +0100

    MOD:

--- cfg/simple_node_cfg.py
+++ cfg/simple_node_cfg.py
@@ -1,7 +1,8 @@
 Node('simple_node',  # a globally unique identification
-     'Simplenode for test purpose',  # describes the node
+     'Simplenode for test purpose\n\n',  # describes the node
       'tcp://10804',
-      implementor = 'Rachit')  # you might choose any port number > 1024
+    
+)  # you might choose any port number > 1024
 Mod('simplenode',  # the name of the module
     'frappy_Rachit.simple_node.SimpleNode',  # the class used for communication
     'SimpleNode',  # a description
